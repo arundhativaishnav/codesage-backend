f640cdc87af07aa7b03f32278294e97e57f1ac11
arundhativaishnav <arundhatianil17@gmail.com>
2025-06-11 01:18:47 +0530

    fixed app.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/App"); // ✅ Not ./src/App
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:3000`);
-});
