f18b9430c67fd7e2a2d423d0c79e653081c69d9e
arundhativaishnav <arundhatianil17@gmail.com>
2025-06-11 00:55:12 +0530

    server.js is fixed

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./app"); // ✅ Not ./src/App
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:3000`);
 });
